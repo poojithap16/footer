8bc2de1062844a9bda0aa80a180521b480571c79
poojitha ponnumani62 <you@example.com>
2024-04-18 11:21:56 +0530

    make some correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import gasell from '../Assests/gasell.png'
 import logo from '../Assests/em.png'
+import saa from '../Assests/saa.png'
+import fb from '../Assests/fb.png'
+import lg from '../Assests/lg.png'
 
 function Footer() {
     return (
@@ -9,23 +11,20 @@ function Footer() {
             {/* Large screen */}
             <div style={{ overflowX: 'hidden' }} class="d-none d-lg-block d-none">
 
-                <Row style={{ backgroundColor: '#7bc64a' }} className='ps-5 pt-4'>
-                    <Col lg={3}>
-                        <h4 style={{ color: 'white' }}>Kontakt</h4>
-                        <p style={{ color: 'white' }}>Egestam Mattsson El & Data AB</p>
-                        <p style={{ color: 'white' }}>Ridspögatan 15a</p>
-                        <p style={{ color: 'white' }}>213 77 Malmö</p>
-                        <br />
-                        <p style={{ color: 'white' }}>Tel: 040-6170166</p>
-                        <p style={{ color: 'white' }}>E-post:info@emeldata.se</p>
-                        <br />
-                        <p style={{ color: 'white' }}> Integritespolicy</p>
-
-                    </Col>
+                <Row style={{ backgroundColor: '#7bc64a' }} className=' pt-4'>
+                <Col lg={3} className='ps-5'>
+                <h3 style={{ color: 'white' }}>Kontakt</h3>
+                <p style={{ color: 'white',marginTop: '15px'  }}>Egestam Mattsson El & Data AB</p>
+                <p style={{ color: 'white'  }}>Ridspögatan 15a</p>
+                <p style={{ color: 'white', marginBottom: '25px' }}>213 77 Malmö</p>
+               <p style={{ color: 'white', }}>Tel: 040-6170166</p>
+                <p style={{ color: 'white', marginBottom: '25px' }}>E-post:  info@emeldata.se</p>
+               <p style={{ color: 'white',  }}>Integritespolicy</p>
+               </Col>
                     <Col lg={2} className='d-flex flex-column'>
-                        <h4 style={{ visibility: 'hidden' }}>categories</h4>
-                        <p style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</p>
-                        <p style={{ color: 'white' }}>Hem</p>
+                        <h3 style={{ visibility: 'hidden' }}>categories</h3>
+                        <h3 style={{  color: 'white', fontSize: '17px',marginTop: '5px' }}>Meny</h3>
+                        <p style={{ color: 'white',marginTop: '5px' }}>Hem</p>
                         <p style={{ color: 'white' }}>Hȧllbarhet</p>
                         <p style={{ color: 'white' }}>Tjänter</p>
                         <p style={{ color: 'white' }}>Kontakta oss</p>
@@ -34,41 +33,42 @@ function Footer() {
 
                     </Col>
                     <Col lg={2} className='d-flex flex-column'>
-                        <h4 style={{ visibility: 'hidden' }}>categories</h4>
+                        <h3 style={{ visibility: 'hidden' }}>categories</h3>
                         <p style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Din integritet</p>
                         <p style={{ color: 'white' }}>Integritetspolicy</p>
                         <p style={{ color: 'white' }}>Cookiepolicy</p>
                         <div className='d-flex'>
-                            <i style={{ color: 'white' }} class="fa-brands fa-facebook fs-4"></i>
-                            <p style={{ color: 'white', paddingLeft: '10px' }}>Facebook</p>
+                            {/* <i style={{ color: 'white' }} class="fa-brands fa-facebook fs-4"></i> */}
+                            <img style={{ width: '32px',height:'32px',top:'117px',left:'204px', marginRight: '5px' }} src={fb} alt="fb" />
+                            <p style={{ color: 'white', paddingLeft: '2%',marginTop:'1%' }}>Facebook</p>
                         </div>
 
                     </Col>
                     <Col lg={3} className='d-flex flex-column'>
-                        <h4 style={{ color: 'white' }}>Gasellvinnare</h4>
-                        <img width={'200px'} height={'100px'} src={gasell} alt="gasell" />
+                        <h3 style={{ color: 'white' }}>Gasellvinnare</h3>
+                        <img width={'200px'} height={'100px'} src={saa} alt="gasell" />
                         <p style={{ color: 'white' }}>Med målet att vara marknadsledande, kombinerar Egestam och Mattsson expertkunskap, engagemang och innovation för att bygga varaktiga kundrelationer och leverera arbete med fokus på kvalitet, miljömedvetenhet och säkerhet.</p>
 
 
                     </Col>
                     <Col lg={2} className='d-flex flex-column'>
                         <h4 style={{ visibility: 'hidden' }}>categories</h4>
-                        <img width={'150px'} height={'100px'} className='d-flex pt-3' src={logo} alt="" />
+                        <img width={'164px'} height={'104px'} top={'146px'} left={'1175px'} className='d-flex pt-3' src={lg} alt="" />
 
                     </Col>
                     <div className='d-flex justify-content-center align-items-center flex-column'>
-                        <hr className='text-white w-50' />
-                        <p className='text-center text-white mt-3'>Copyright &#169; 2024 All rights reserved.</p>
+                    <hr className='text-white w-100 '  />                                
+                       <p className='text-center text-white mt-3'>Copyright &#169; 2024 All rights reserved.</p>
                     </div>
                 </Row>
 
             </div>
             {/* Medium Screen */}
             <div style={{ overflowX: 'hidden' }} className="d-none d-md-block d-lg-none">
-                <Row className='ps-5' style={{ backgroundColor: '#7bc64a', paddingTop: '100px' }}>
+                <Row className='ps-4' style={{ backgroundColor: '#7bc64a', paddingTop: '100px' }}>
                     <Col md={6}>
                         <Row>
-                            <h4 style={{ color: 'white' }}>Kontakt</h4>
+                            <h3 style={{ color: 'white' }}>Kontakt</h3>
                             <div style={{ lineHeight: '0.5', color: 'white', marginTop: '10px' }}>
                                 <p>Egestam Mattsson El & Data AB</p>
                                 <p>Ridspögatan 15a</p>
@@ -78,21 +78,20 @@ function Footer() {
 
                             <div style={{ lineHeight: '0.5', color: 'white' }}>
                                 <p className='mt-4'>Tel: 040-6170166</p>
-                                <p>E-post:info@emeldata.se</p>
+                                <p>E-post: info@emeldata.se</p>
                             </div>
 
                         </Row>
-                        <hr className='text-white' />
-
+                        <hr className='text-white' style={{ position: 'absolute', left: 0, width: '50%' }} />
                         <Row className='mt-4'>
                             <Col md={6}>
-                                <p style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</p>
+                                <h3 style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</h3>
                                 <div style={{ color: 'white' }} >
-                                    <p>Hem</p>
-                                    <p>Hȧllbarhet</p>
-                                    <p>Tjänter</p>
-                                    <p>Kontakta oss</p>
-                                    <p>Jobba med oss</p>
+                                <p>Vårt erbjudande</p>
+                                <p>Hållbarhet</p>
+                                <p>Jobba hos oss</p>
+                                <p>Om oss</p>
+                                <p>Kontakta oss</p>
                                 </div>
                             </Col>
                             <Col md={6}>
@@ -100,10 +99,11 @@ function Footer() {
                                 <div style={{ color: 'white' }} >
                                     <p>Integritetspolicy</p>
                                     <p>Cookiepolicy</p>
-                                    <div className='d-flex'>
-                                        <i style={{ color: 'white' }} class="fa-brands fa-facebook fs-4"></i>
-                                        <p style={{ color: 'white', paddingLeft: '10px' }}>Facebook</p>
-                                    </div>
+                                    <div style={{ paddingTop: '2px', marginBottom: '20px',marginTop:'-2%' }}>  <i style={{ color: 'white' }} className="fa-brands fa-facebook fs-2"></i>
+                                    <div style={{ marginLeft: '40px' }}>
+    <p style={{ color: '#FFFFFF', marginBottom: '10px',marginTop:'-12%',marginLeft:'1%', font:'Inter',size:'16px' }}>Facebook
+    </p>
+  </div></div>
 
                                 </div>
                             </Col>
@@ -116,12 +116,12 @@ function Footer() {
             </div>
             {/* Small screen */}
             <div style={{ overflowX: 'hidden' }} className='d-none d-sm-block d-md-none'>
-                <Row className='pt-3 ps-4' style={{ backgroundColor: '#7bc64a' }}>
+                <Row className='pt-3 ' style={{ backgroundColor: '#7bc64a' }}>
 
 
-                    <Row >
+                    <Row className='ps-5' >
                         <img style={{width:'200px' , height:'100px'}} src={logo} alt="" />
-                        <h4 style={{ color: 'white', paddingTop: '20px' }}>Kontakt</h4>
+                        <h3 style={{ color: 'white', paddingTop: '20px' }}>Kontakt</h3>
                         <div style={{ lineHeight: '0.5', color: 'white', marginTop: '10px' }}>
                             <p>Egestam Mattsson El & Data AB</p>
                             <p>Ridspögatan 15a</p>
@@ -131,19 +131,19 @@ function Footer() {
 
                         <div style={{ lineHeight: '0.5', color: 'white' }}>
                             <p className='mt-4'>Tel: 040-6170166</p>
-                            <p>E-post:info@emeldata.se</p>
+                            <p>E-post:  info@emeldata.se</p>
                         </div>
                     </Row>
-                    <hr className='text-white' />
-                    <Row>
+                    <hr className='text-white ' style={{ width: '87%', marginRight: '25%' }} />
+                    <Row className='ps-5'>
                         <Col xs={6}>
-                            <p style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</p>
+                            <h3 style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</h3>
                             <div style={{ color: 'white' }} >
-                                <p>Hem</p>
-                                <p>Hȧllbarhet</p>
-                                <p>Tjänter</p>
+                                <p>Vårt erbjudande</p>
+                                <p>Hållbarhet</p>
+                                <p>Jobba hos oss</p>
+                                <p>Om oss</p>
                                 <p>Kontakta oss</p>
-                                <p>Jobba med oss</p>
                             </div>
                         </Col>
                         <Col xs={6}>
@@ -165,12 +165,12 @@ function Footer() {
             </div>
             {/* Extra Small screen */}
             <div style={{ overflowX: 'hidden' }} className='d-block d-sm-none'>
-                <Row className='pt-3 ps-4' style={{ backgroundColor: '#7bc64a' }}>
+                <Row className='pt-3 ' style={{ backgroundColor: '#7bc64a' }}>
 
 
-                    <Row >
+                    <Row className='ps-5' >
                         <img style={{width:'200px' , height:'100px'}} src={logo} alt="" />
-                        <h4 style={{ color: 'white', paddingTop: '20px' }}>Kontakt</h4>
+                        <h3 style={{ color: 'white', paddingTop: '20px' }}>Kontakt</h3>
                         <div style={{ lineHeight: '0.5', color: 'white', marginTop: '10px' }}>
                             <p>Egestam Mattsson El & Data AB</p>
                             <p>Ridspögatan 15a</p>
@@ -180,19 +180,19 @@ function Footer() {
 
                         <div style={{ lineHeight: '0.5', color: 'white' }}>
                             <p className='mt-4'>Tel: 040-6170166</p>
-                            <p>E-post:info@emeldata.se</p>
+                            <p>E-post:  info@emeldata.se</p>
                         </div>
                     </Row>
-                    <hr className='text-white' />
-                    <Row>
+                    <hr className='text-white ' style={{ width: '87%', marginRight: '25%' }}  />
+                    <Row className='ps-5'>
                         <Col xs={6}>
-                            <p style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</p>
+                            <h3 style={{ fontWeight: 'bold', color: 'white', fontSize: '17px' }}>Meny</h3>
                             <div style={{ color: 'white' }} >
-                                <p>Hem</p>
-                                <p>Hȧllbarhet</p>
-                                <p>Tjänter</p>
+                            <p>Vårt erbjudande</p>
+                                <p>Hållbarhet</p>
+                                <p>Jobba hos oss</p>
+                                <p>Om oss</p>
                                 <p>Kontakta oss</p>
-                                <p>Jobba med oss</p>
                             </div>
                         </Col>
                         <Col xs={6}>
